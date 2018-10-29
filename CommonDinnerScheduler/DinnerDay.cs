15ac4a60096888ac8cb3b638f9100b97ce379c01
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-10-29 11:46:20 +0100

    Initial commit

---
+++ CommonDinnerScheduler/DinnerDay.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonDinnerScheduler
+{
+    public class DinnerDay
+    {
+        private List<String> Participants;
+        private DayOfWeek dayOfWeek { get; set; }
+        private DateTime startDate { get; set; }
+        private DateTime endDate { get; set; }
+
+        public DinnerDay(DayOfWeek weekDay, DateTime StartDate, DateTime EndDate)
+        {
+            Participants = new List<string>();
+            dayOfWeek = weekDay;
+            startDate = StartDate;
+            endDate = EndDate;
+        }
+
+        public bool AddParticipant(String name)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            //If name is not yet on list, add it
+            if (!Participants.Contains(name))
+            {
+                Participants.Add(name);
+                //Indicate that name has been added.
+                return true;
+            }
+            else
+            {
+                //Indicate that name has not been added, since it's already on the list
+                return false;
+            }
+        }
+
+        public bool RemoveParticipant(String name)
+        {
+            //Check if name is in list
+            if (Participants.Contains(name))
+            {
+                //Remove from list
+                Participants.Remove(name);
+                //return true, indicating name was removed
+                return true;
+            }
+            else
+            {
+                //return false, indicating name was not removed
+                return false;
+            }
+        }
+
+        public int GetNumberOfParticipants()
+        {
+            return Participants.Count;
+        }
+    }
+}
---
+++ DinnerDay.UnitTests/DinnerDay.UnitTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CommonDinnerScheduler;
+
+namespace DinnerDay_UnitTests
+{
+    [TestFixture]
+    public class DinnerDayUnitTest
+    {
+        private DinnerDay uut;
+        private DateTime today;
+        DateTime threeWeeksFromNow;
+
+        [SetUp]
+        public void Setup()
+        {
+            today = DateTime.Today;
+            threeWeeksFromNow=DateTime.Today.AddDays(3*7);
+            uut = new DinnerDay(DayOfWeek.Monday, today, threeWeeksFromNow);
+        }
+
+        [Test]
+        public void Participants_NoneAddedGetNumber_Returns0()
+        {
+            Assert.That(()=>uut.GetNumberOfParticipants().Equals(0));
+        }
+
+        [Test]
+        public void Participants_Add1GetNumber_Returns1()
+        {
+            uut.AddParticipant("340");
+            Assert.That(() => uut.GetNumberOfParticipants().Equals(1));
+        }
+
+        [Test]
+        public void Participants_Add2DifferentGetNumber_Returns2()
+        {
+            uut.AddParticipant("340");
+            uut.AddParticipant("341");
+            Assert.That(() => uut.GetNumberOfParticipants().Equals(2));
+        }
+
+        [Test]
+        public void Participants_Add2SameGetNumber_Returns1()
+        {
+            uut.AddParticipant("340");
+            uut.AddParticipant("340");
+            Assert.That(() => uut.GetNumberOfParticipants().Equals(1));
+        }
+
+        [Test]
+        public void Participants_AddExisting_ReturnsFalse()
+        {
+            uut.AddParticipant("340");
+            Assert.That(() => uut.AddParticipant("340").Equals(false));
+        }
+
+        [Test]
+        public void Participants_AddDifferent_ReturnsTrue()
+        {
+            uut.AddParticipant("340");
+            Assert.That(() => uut.AddParticipant("341").Equals(true));
+        }
+    } 
+}
